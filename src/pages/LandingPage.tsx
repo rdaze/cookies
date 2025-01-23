369be6ab1dd1bc1a5fa6dc418aad528be3f931db
Max Keller <m_keller@stud.uni-bamberg.de>
2025-01-23 19:23:43 +0100

    another 404 fix

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -24,10 +24,10 @@ const LandingPage: React.FC = () => {
             </p>
 
             <div className="button-container">
-                <button onClick={() => window.open("/#/group1", "_blank")}>Open Group 1</button>
-                <button onClick={() => window.open("/#/group2", "_blank")}>Open Group 2</button>
-                <button onClick={() => window.open("/#/group3", "_blank")}>Open Group 3</button>
-                <button onClick={() => window.open("/#/group4", "_blank")}>Open Group 4</button>
+                <button onClick={() => window.open("/cookies/#/group1", "_blank")}>Open Group 1</button>
+                <button onClick={() => window.open("/cookies/#/group2", "_blank")}>Open Group 2</button>
+                <button onClick={() => window.open("/cookies/#/group3", "_blank")}>Open Group 3</button>
+                <button onClick={() => window.open("/cookies/#/group4", "_blank")}>Open Group 4</button>
             </div>
 
             <TriviaComponent />
