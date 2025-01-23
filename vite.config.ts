7db32caf3220a67e39ff60f0070458a057151b26
Max Keller <m_keller@stud.uni-bamberg.de>
2025-01-23 19:00:31 +0100

    fix 404

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cookies/', // Correct base path for GitHub Pages
+  base: '/rdaze.github.io/', // Correct base path for GitHub Pages
   build: {
     outDir: 'dist', // Ensures GitHub Pages picks up the right folder
   }
